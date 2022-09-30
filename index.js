b98bb13bd931060414990782af4d3e61dbe7c227
TimLuq <timluq+gitcommit@gmail.com>
2022-09-30 10:16:37 +0200

    build

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export{SerializationContext}from"./es-serializer.js";import"./es5.js";import"./es2015.js";import"./with-statement-6f00ebc1.js";
+export{SerializationContext}from"./es-serializer.js";import"./es5.js";import"./es2015.js";import"./with-statement-225b4b90.js";
 //# sourceMappingURL=index.js.map
