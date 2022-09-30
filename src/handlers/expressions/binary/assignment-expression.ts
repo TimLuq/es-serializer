804c3c328192bad48356ce5a442ce8aac339dac9
TimLuq <timluq+gitcommit@gmail.com>
2022-09-30 10:15:52 +0200

    fix assignment indentation

--- src/handlers/expressions/binary/assignment-expression.ts
+++ src/handlers/expressions/binary/assignment-expression.ts
@@ -4,10 +4,6 @@ import { ISerializationOptions, ISerializationResult, ISerializationContext } fr
 export function AssignmentExpression(context: ISerializationContext, ast: import("estree").AssignmentExpression, opts: ISerializationOptions): ISerializationResult {
     const d = opts.indent;
     const indent = d !== undefined;
-    if (indent) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        opts.indent!++;
-    }
     const left = context.serialize(ast.left, opts).code;
     const right = context.serialize(ast.right, opts).code;
     let code = left;
